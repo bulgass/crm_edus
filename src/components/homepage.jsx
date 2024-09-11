4fd8bc1df12dea9b4340bde5b665d38e12ab096f
bulgass <bswcourses@gmail.com>
2024-09-11 21:22:19 +0600

    Added homepage

---
+++ src/components/homepage.jsx
@@ -0,0 +1,12 @@
+
+import HomePage from './homepageComponent';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <HomePage/> 
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/components/homepageComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './homepage.css'; 
+
+
+const HomePage = () => {
+
+
+  return (
+    <div className="home-page">
+        Hello world
+        </div>
+  );
+};
+
+export default HomePage;
