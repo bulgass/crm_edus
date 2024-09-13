353d3d2cf604cf8abd7875478a1ffcd51bda1286
bulgass <bswcourses@gmail.com>
2024-09-13 21:23:44 +0600

    little changes

--- src/components/pages/inbox/inboxComponent.jsx
+++ src/components/pages/inbox/inboxComponent.jsx
@@ -87,7 +87,8 @@ const InboxComponent = () => {
       <section className="MainContent">
         <Tabs value={activeTab} onChange={handleTabChange}>
           <Tab label="Income" />
-          {userRole === 'admin' && <Tab label="Sent" />}
+          {userRole === 'admin' && <Tab label="Send" />}
+          <Tab label="Sent"></Tab>
           <Tab label="Trash" />
         </Tabs>
         <div className="MessageBlock">
@@ -153,7 +154,7 @@ const InboxComponent = () => {
                 onClick={handleSendMessage}
                 style={{ marginTop: 10 }}
               >
-                Send
+                
               </Button>
             </div>
           )}
