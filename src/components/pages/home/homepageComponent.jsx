b3553281387865f92a004cdfb514666d093ddd34
KutmanBekbolotov <kutmanbekbolotov2003@gmail.com>
2024-09-13 21:21:31 +0600

    edit styles

--- src/components/pages/home/homepageComponent.jsx
+++ src/components/pages/home/homepageComponent.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { MenuBar } from '../../submodules/menuBar/menubar';
+import './homepage.css'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
+    <div className="homepageComponent">
+      <MenuBar/>
+      <div className='header_home'>
+        Hello wolrd
+      </div>
     </div>
   );
 };
